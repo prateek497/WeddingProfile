0379b4c2b4bf17f313394d0ce34a3705d9c259b4
prateek calpion <pgangwar@CPN-IT-PRATEEK.calpionindia.com>
2016-08-17 18:56:15 +0530

    getting html code from UI

--- Biodata/biodata/Controllers/HomeController.cs
+++ Biodata/biodata/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace biodata.Controllers
 {
@@ -10,17 +12,26 @@ namespace biodata.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(new ViewModel() { ClientHTML = new StringBuilder() });
         }
 
         [HttpPost]
-        public ActionResult GenaratePdf(string htmlcontent)
+        [ValidateInput(false)]
+        public ActionResult GenaratePdf(string ClientHTML, ViewModel model)
         {
-            //render partial view as string
-            //var pdfBytes = (new NReco.PdfGenerator.HtmlToPdfConverter()).GeneratePdf(htmlcontent);
 
-            return null;
+            //render partial view as string
+           // var pdfBytes = (new NReco.PdfGenerator.HtmlToPdfConverter()).GeneratePdf(ClientHTML);
+           // System.IO.File.WriteAllBytes(@"E:\Practice\biodata\Biodata\testpdf.pdf", pdfBytes);
+            return Content(ClientHTML);
         }
 
     }
+
+    public class ViewModel
+    {
+        [AllowHtml]
+        public StringBuilder ClientHTML { get; set; }
+    }
+
 }
