6bbd16917bf2cd2ef7fced40ad38ca0ad574981d
prateek calpion <pgangwar@CPN-IT-PRATEEK.calpionindia.com>
2016-08-25 20:48:29 +0530

    Render url

--- Biodata/biodata/Controllers/HomeController.cs
+++ Biodata/biodata/Controllers/HomeController.cs
@@ -20,6 +20,8 @@ namespace biodata.Controllers
         [ValidateInput(false)]
         public ActionResult GenaratePdf(string ClientHTML, ViewModel model)
         {
+            //var rawHtml = Server.HtmlDecode(ClientHTML);
+
             var pdfBytes = (new HtmlToPdfConverter { Margins = new PageMargins { Top = 0, Bottom = 0, Left = 0, Right = 0 } }).GeneratePdf(ClientHTML);
             System.IO.File.WriteAllBytes(@"E:\Practice\biodata\Biodata\testpdf.pdf", pdfBytes);
 
@@ -39,6 +41,18 @@ namespace biodata.Controllers
         public PartialViewResult _Basic()
         {
             return PartialView(new ViewModel { ClientHTML = new StringBuilder() });
+        }
+
+        [HttpPost]
+        public ActionResult _Basic(string model)
+        {
+            var htmlToPdf = new HtmlToPdfConverter
+            {
+                Margins = new PageMargins { Top = 0, Bottom = 0, Left = 0, Right = 0 },
+                Size = PageSize.A4
+            };
+            htmlToPdf.GeneratePdfFromFile("http://localhost:49183/Home/_Basic", null, @"E:\Practice\biodata\Biodata\testpdf.pdf");
+            return null;
         }
 
     }
