3aa9fb8836630787ce24de5f7cec69ea8bfbdcb8
Prateek Gangwar <prateek.gangwar@outlook.com>
2016-09-03 16:45:28 +0530

    project clean up

--- Biodata/biodata/Controllers/HomeController.cs
+++ Biodata/biodata/Controllers/HomeController.cs
@@ -11,56 +11,17 @@ namespace biodata.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ViewResult _Basic()
         {
-            return View(new ViewModel() { ClientHTML = new StringBuilder() });
-        }
-
-        [HttpPost]
-        [ValidateInput(false)]
-        public ActionResult GenaratePdf(string ClientHTML, ViewModel model)
-        {
-            //var rawHtml = Server.HtmlDecode(ClientHTML);
-
-            var pdfBytes = (new HtmlToPdfConverter { Margins = new PageMargins { Top = 0, Bottom = 0, Left = 0, Right = 0 } }).GeneratePdf(ClientHTML);
-            //System.IO.File.WriteAllBytes(@"E:\Practice\biodata\Biodata\testpdf.pdf", pdfBytes);
-
-            //E:\Practice\biodata\Biodata\testpdf.pdf  D:\testpdf.pdf
-
-            //var htmlToPdf = new HtmlToPdfConverter
-            //{
-            //    Margins = new PageMargins { Top = 0, Bottom = 0, Left = 0, Right = 0 },
-            //    Size = PageSize.A4
-            //};
-            //htmlToPdf.GeneratePdfFromFile("http://datecalculator.in/", null, @"D:\testpdf.pdf");
-
-            return Content(ClientHTML);
-        }
-
-
-        public PartialViewResult _Basic()
-        {
-            return PartialView(new ViewModel { ClientHTML = new StringBuilder() });
+            return View();
         }
 
         [HttpPost]
         public ActionResult _Basic(string model)
         {
-            var htmlToPdf = new HtmlToPdfConverter
-            {
-                Margins = new PageMargins { Top = 0, Bottom = 0, Left = 0, Right = 0 },
-                Size = PageSize.A4
-            };
-            htmlToPdf.GeneratePdfFromFile("http://localhost:49183/Home/_Basic", null, @"D:\testpdf.pdf");
+            PDFGenerator.PdfGenerator.Generate("http://localhost:49183/Home/_Basic", "D:\testpdf.pdf");
+
             return RedirectToAction("_Basic");
         }
-
     }
-
-    public class ViewModel
-    {
-        [AllowHtml]
-        public StringBuilder ClientHTML { get; set; }
-    }
-
 }
