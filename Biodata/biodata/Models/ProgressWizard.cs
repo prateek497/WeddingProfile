ebb214e4dffe863ba98a70f9faf6503875532a92
prateek calpion <prateek497@gmail.com>
2017-01-04 17:20:31 +0530

    Added dynamic progress wizard

---
+++ Biodata/biodata/Models/ProgressWizard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace biodata.Models
+{
+    public class ProgressWizard
+    {
+        public int Step { get; set; }
+    }
+}
