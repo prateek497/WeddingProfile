55768f73820aee0197754f54b6f4c30cd25d3c90
prateek calpion <prateek497@gmail.com>
2017-01-21 11:16:40 +0530

    files added

---
+++ Biodata/biodata/Models/Education.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace biodata.Models
+{
+    public class Education
+    {
+        public string BachelorCollegeOrUni { get;  set; }
+
+        public string BachelorPassoutYear { get; set; }
+
+        public string BachelorDegree { get; set; }
+
+        public string PgCollegeOrUni { get; set; }
+
+        public string PgPassoutYear { get; set; }
+
+        public string PgDegree { get; set; }
+    }
+}
---
+++ Biodata/biodata/Models/Religious.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace biodata.Models
+{
+    public class Religious
+    {
+        public string Religion { get; set; }
+
+        public string Caste { get; set; }
+
+        public string Gotra { get; set; }
+
+        public string Zodiac { get; set; }
+
+        public string Languages { get; set; }
+    }
+}
